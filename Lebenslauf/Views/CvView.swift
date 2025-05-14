9279f057f5b606a6eeabbfb7124c1b409a472418
Rylie Castell <rylie.castell@gmail.com>
2025-05-14 13:57:11 +0200

    add styling to header

--- Lebenslauf/Views/CvView.swift
+++ Lebenslauf/Views/CvView.swift
@@ -11,13 +11,23 @@ struct CvView: View {
     var body: some View {
         VStack {
             Text("Hi, my name is Rylie.")
-                .font(.title)
-            Text("Developer")
-                .font(.largeTitle)
-            Text("😎")
+                .font(.custom("AvenirNext-Medium", size: 22))
+                .offset(x: -52)
+            ZStack {
+                Image("Home_Page")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 264.12, height: 286)
+                    .offset(x: -90, y: 20)
+                    .zIndex(1)
+                Text("DEVELOPER")
+                    .font(.custom("DINCondensed-Bold", size: 88))
+                    .padding(.top, -150)
+                    .zIndex(0)
+            }
             Spacer()
         }
-        
+
     }
 }
 
