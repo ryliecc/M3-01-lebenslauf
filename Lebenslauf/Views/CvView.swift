7edf48d084fa627136d6889db24fd8b08b9d71b9
Rylie Castell <rylie.castell@gmail.com>
2025-05-14 13:58:55 +0200

    add header view

--- Lebenslauf/Views/CvView.swift
+++ Lebenslauf/Views/CvView.swift
@@ -9,25 +9,7 @@ import SwiftUI
 
 struct CvView: View {
     var body: some View {
-        VStack {
-            Text("Hi, my name is Rylie.")
-                .font(.custom("AvenirNext-Medium", size: 22))
-                .offset(x: -52)
-            ZStack {
-                Image("Home_Page")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 264.12, height: 286)
-                    .offset(x: -90, y: 20)
-                    .zIndex(1)
-                Text("DEVELOPER")
-                    .font(.custom("DINCondensed-Bold", size: 88))
-                    .padding(.top, -150)
-                    .zIndex(0)
-            }
-            Spacer()
-        }
-
+        HeaderView()
     }
 }
 
